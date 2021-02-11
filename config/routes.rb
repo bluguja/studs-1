5bd1ed2b37ee8a80b91d45d28c98cc3289d2c717
Bala Luguja <bluguja@gmail.com>
2021-02-11 11:17:26 -0700

    sample .env files

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :schedules
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+  resources :posts
+  root 'posts#index'
+end
 end
