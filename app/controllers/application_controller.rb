4f6b44484891fab5a24279775bb909bb70e220e1
Bala Luguja <bluguja@gmail.com>
2021-02-11 20:24:49 -0700

    app controller update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :valid_user
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :type])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :type])
+    end
+
+    def after_sign_in_path_for(user)
+        if user.student?
+            schedules_path
+        elsif user.teacher?
+            students_path
+        else
+            edit_user_registration_path
+        end
+    end
+
+    def valid_user
+        if current_user && current_user.undefined?
+            redirect_to edit_user_registration_path && return
+        end
+    end
+end
+
 end
