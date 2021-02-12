0ae59f40a3f6a978de7706b574adfb19f6678ddf
Bala Luguja <bluguja@gmail.com>
2021-02-11 22:45:04 -0700

    updated devise_for in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :schedules
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   devise_for :users, controllers: { omniauth_callbacks: 'omniauth'}
 
-  Rails.application.routes.draw do
-  resources :posts
-  root 'posts#index'
-end
+  resources :home
+  resources :schedules
+  resources :teacherss do
+    resources :schedules
+  end
+  resources :patients
+  root 'home#index'
 end
